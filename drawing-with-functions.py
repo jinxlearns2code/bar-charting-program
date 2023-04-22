f48940b028321982d251a9e92ba46b1d07244bc7
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-04-23 01:34:19 +1000

    function for y axis ticks added

--- drawing-with-functions.py
+++ drawing-with-functions.py
@@ -18,6 +18,7 @@ turtle.hideturtle()
 # Main function
 def main():
     axes()
+    ticks()
 
 
 # Function to draw axes
@@ -37,6 +38,36 @@ def axes():
     turtle.forward(400)
     turtle.stamp()
     turtle.right(180)
+
+
+# Function to draw tick marks for y-axis
+def ticks():
+    total = 0
+    turtle.pensize(width=3)
+    turtle.penup()
+    turtle.goto(STARTING_POINT, STARTING_POINT)
+    turtle.pendown()
+    for i in range(Y_TICK_MARKS-1):
+        turtle.setheading(180)
+        turtle.forward(8)
+        turtle.penup()
+        turtle.forward(14)
+        turtle.setheading(270)
+        turtle.forward(6)
+        turtle.write(total)
+        total = total + 6
+        turtle.back(6)
+        turtle.setheading(0)
+        turtle.forward(22)
+        turtle.pendown()
+        turtle.setheading(90)
+        turtle.forward(78)
+    turtle.penup()
+    turtle.setheading(180)
+    turtle.forward(22)
+    turtle.setheading(270)
+    turtle.forward(6)
+    turtle.write(30)
     turtle.done()
 
 
