5e88b8ab982e5120efa11af74eb858692f549706
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-04-23 16:04:26 +1000

    added a feature to store user's input

--- drawing-with-functions.py
+++ drawing-with-functions.py
@@ -47,28 +47,35 @@ def ticks():
     turtle.penup()
     turtle.goto(STARTING_POINT, STARTING_POINT)
     turtle.pendown()
-    for i in range(Y_TICK_MARKS-1):
+    for i in range(Y_TICK_MARKS):
         turtle.setheading(180)
         turtle.forward(8)
         turtle.penup()
         turtle.forward(14)
         turtle.setheading(270)
         turtle.forward(6)
-        turtle.write(total)
+        turtle.write(total, align='center', font=('Arial', 10, 'normal'))
         total = total + 6
         turtle.back(6)
         turtle.setheading(0)
         turtle.forward(22)
-        turtle.pendown()
         turtle.setheading(90)
         turtle.forward(78)
-    turtle.penup()
-    turtle.setheading(180)
-    turtle.forward(22)
-    turtle.setheading(270)
-    turtle.forward(6)
-    turtle.write(30)
+        turtle.pendown()
     turtle.done()
+
+
+# Ask user for input - data points and individual data values
+arr = []
+value = int(turtle.numinput('Data Points', 'Please enter the total number of data points (0-20):', minval=0, maxval=20))
+for num in range(value):
+    array_item = int(turtle.numinput("Data Values", "Please enter data " + str(num+1) + "'s" + " value (0-30):",
+                                     minval=0, maxval=30))
+    arr.append(array_item)
+
+
+# Draw bar chart/s using the data provided by the user (stored in 'arr' array)
+
 
 
 main()
